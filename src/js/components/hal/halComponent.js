6d4d69acc9990d208b2b340e775f5c8176bb86f6
Marek Gańko <ganko.marek@gmail.com>
2015-07-01 00:17:34 +0200

    small fixes

--- src/js/components/hal/halComponent.js
+++ src/js/components/hal/halComponent.js
@@ -29,13 +29,14 @@ componentsModule.directive('pifHalComponent', (HalSpeak, HalListen, nlp, $http) 
       function talkWithHal() {
         return HalListen.listen()
           .then((result) => {
-
+            halTextElement[0].innerHTML = '...';
             $http.get(`https://yoda.p.mashape.com/yoda?sentence=${encodeURI(result)}`, {
               headers: {'X-Mashape-Key': 'YuX5mt727rmsh2PDPTHIySgjnyJvp1hp1BQjsnFkRVicjDsYoB', 'Accept': 'text/plain'}
             }).then((result) => {
               console.log(result.data);
 
-              HalSpeak.say('You said: ' + result.data, halTextElement);
+              //HalSpeak.say('You said: ' + result.data, halTextElement);
+              HalSpeak.say('Yoda would say: ' + result.data, halTextElement);
               talkWithHal('Please tell me something else');
             });
 
